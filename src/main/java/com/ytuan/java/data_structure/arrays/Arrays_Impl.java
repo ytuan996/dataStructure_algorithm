2aec6220b166a013e25654411fe1da1a6d86b5ae
ytuan996 <ytuan996@163.com>
2019-05-22 10:52:27 -0700

    cd

--- src/main/java/com/ytuan/java/data_structure/arrays/Arrays_Impl.java
+++
@@ -1,5 +0,0 @@
-package com.ytuan.java.data_structure.arrays;
-
-public class Arrays_Impl implements Arrays_interface {
-
-}
--- src/main/java/com/ytuan/java/data_structure/arrays/Arrays_interface.java
+++
@@ -1,6 +0,0 @@
-package com.ytuan.java.data_structure.arrays;
-
-public interface Arrays_interface {
-
-}
-
