89a854f4399a9e6bd0e10f7e9c1e49b120eaa436
ytuan996 <ytuan996@163.com>
2019-05-22 10:41:45 -0700

    first commit form local computer!

---
+++ src/main/java/com/ytuan/java/data_structure/arrays/Arrays_Impl.java
@@ -0,0 +1,5 @@
+package com.ytuan.java.data_structure.arrays;
+
+public class Arrays_Impl implements Arrays_interface {
+
+}
---
+++ src/main/java/com/ytuan/java/data_structure/arrays/Arrays_interface.java
@@ -0,0 +1,6 @@
+package com.ytuan.java.data_structure.arrays;
+
+public interface Arrays_interface {
+
+}
+
