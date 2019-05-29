af3cc16902fab4cf78a451f5d36ee6a093e59c9c
ytuan996 <ytuan996@163.com>
2019-05-28 22:39:54 -0700

    修改了最大堆接口

--- dataStructure_algorithm/src/main/java/com/ytuan/java/data_structure/heap/MaxHeap_interface.java
+++ dataStructure_algorithm/src/main/java/com/ytuan/java/data_structure/heap/MaxHeap_interface.java
@@ -2,6 +2,10 @@ package com.ytuan.java.data_structure.heap;
 
 public interface MaxHeap_interface<E extends Comparable> {
 
+    int getSize();
+
+    boolean isEmpty();
+
     void add(E element);
 
     E getMax();
