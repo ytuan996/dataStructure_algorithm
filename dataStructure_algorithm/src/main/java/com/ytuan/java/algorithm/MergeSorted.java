75a96d82d2de08227f39399feccb01008e930e60
ytuan996 <ytuan996@163.com>
2019-08-30 15:06:36 +0800

    归并排序

--- dataStructure_algorithm/src/main/java/com/ytuan/java/algorithm/MergeSorted.java
+++ dataStructure_algorithm/src/main/java/com/ytuan/java/algorithm/MergeSorted.java
@@ -1,6 +1,39 @@
 package com.ytuan.java.algorithm;
 
+/**
+ * <p>
+ * 归并排序
+ * </p>
+ * <p>
+ * 基本思想：
+ * </p>
+ * <p>
+ * 时间复杂度分析：在两两合并的时候，需要扫描一遍数组，复杂度为O(n),
+ * 归并排序的过程中，根据完全二叉树的性质，需要合并log2n次，所以总体的复杂度为 O(nlogn)
+ * </p>
+ *
+ * @Author: ytuan
+ * @Date: 2019-08-30  00:27
+ */
 public class MergeSorted {
+
+    public static void main(String[] args) {
+
+        MergeSorted mergeSorted = new MergeSorted();
+
+        // 30, 29,28,27,26,25,24,23,22,21,20, 19, 18, 17, 16,15, 14, 13, 12, 11, 10, 9, 8, 7, 6,
+
+        int arr[] = new int[]{5, 4, 3, 2, 1};
+
+        System.out.print(" ========>原始：");
+
+        ArrayUtils.printArray(arr);
+
+        System.out.println();
+
+        mergeSorted.mergeSorted(arr);
+
+    }
 
     public void mergeSorted(int arr[]) {
 
@@ -63,21 +96,4 @@ public class MergeSorted {
         }
     }
 
-    public static void main(String[] args) {
-
-        MergeSorted mergeSorted = new MergeSorted();
-
-        // 30, 29,28,27,26,25,24,23,22,21,20, 19, 18, 17, 16,15, 14, 13, 12, 11, 10, 9, 8, 7, 6,
-
-        int arr[] = new int[]{5, 4, 3, 2, 1};
-
-        System.out.print(" ========>原始：");
-
-        ArrayUtils.printArray(arr);
-
-        System.out.println();
-
-        mergeSorted.mergeSorted(arr);
-
-    }
 }
